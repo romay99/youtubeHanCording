cc3018ceadfe42c7ba74ad2ef17614594a43d43f
soobin_labtop <i_romay@naver.com>
2022-11-15 20:11:29 +0900

    Initial commit

---
+++ src/main/java/com/youtubeHancording/demo/SubinApplication.java
@@ -0,0 +1,13 @@
+package com.youtubeHancording.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youtubeHancording/demo/SubinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youtubeHancording.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
