479af3e9942a6598daa41ab083d697972e9b569b
SooBin_Lee <i_romay@naver.com>
2022-11-18 00:04:17 +0900

    열심히!

--- src/main/java/com/youtubeHancording/demo/controller/BoardController.java
+++ src/main/java/com/youtubeHancording/demo/controller/BoardController.java
@@ -4,8 +4,10 @@ import com.youtubeHancording.demo.entity.Board;
 import com.youtubeHancording.demo.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BoardController {
@@ -14,13 +16,27 @@ public class BoardController {
     private BoardService boardService;
 
     @GetMapping("/board/write")  // localhost:8080/board/write 접속 url
-    public String boardWriteForm(){
+    public String boardWriteForm() {
         return "boardwrite";
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board){
+    public String boardWritePro(Board board) {
         boardService.write(board);
         return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model) {
+
+        model.addAttribute("list", boardService.boardList());
+
+        return "boardList";
+    }
+
+    @GetMapping("/board/view")
+    public String boardView(Model model, Integer id){
+        model.addAttribute("board",boardService.boardView(id));
+        return "boardview";
     }
 }
--- src/main/java/com/youtubeHancording/demo/entity/Board.java
+++ src/main/java/com/youtubeHancording/demo/entity/Board.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
 @Entity
 @Data
 public class Board {
