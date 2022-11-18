58574dd078eee79c4c03e4823c5568dbd355e811
soobin_labtop <i_romay@naver.com>
2022-11-18 15:14:10 +0900

    add update

--- src/main/java/com/youtubeHancording/demo/controller/BoardController.java
+++ src/main/java/com/youtubeHancording/demo/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -35,8 +36,36 @@ public class BoardController {
     }
 
     @GetMapping("/board/view")
-    public String boardView(Model model, Integer id){
-        model.addAttribute("board",boardService.boardView(id));
+    public String boardView(Model model, Integer id) {
+        model.addAttribute("board", boardService.boardView(id));
         return "boardview";
     }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id) {
+
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("board", boardService.boardView(id));
+
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id")Integer id , Board board){
+
+        Board boardTmp = boardService.boardView(id);
+        boardTmp.setTitle(board.getTitle());
+        boardTmp.setContent(board.getContent());
+        boardService.write(boardTmp);
+
+
+        return "redirect:/board/list";
+    }
+
+
 }
